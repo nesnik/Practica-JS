58f2eee9d4a8fc102463a9d90fdc47a1059c62fb
Nikolas Nesterenko <steepdog3@gmail.com>
2021-07-13 22:10:05 +0300

    feat: add 1 test

---
+++ index1.js
@@ -0,0 +1,15 @@
+// Практика JS 1.
+// Создайте переменную с помощью camelCase, snake_case и PascalCase и
+// присвойте им значения от 0-я до 2-х. Выведите значения этих переменных
+// на экран с помощью метода alert и console.log. Скажите, чем кардинально
+// отличается alert и console.log?
+
+const firstVariable = 0
+const second_variable = 1
+const ThirdVariable = 2
+
+alert(firstVariable, second_variable, ThirdVariable)
+console.log(firstVariable, second_variable, ThirdVariable)
+
+// Кардинальное отличия Alert от console.log тем, что 1-е просто выводит на экран, а во 2-м можно делать разного рода операции с переменными. 
+
