0ff072fcd90581a1d9e5b103d1d55a459dfa8a09
Nikolas Nesterenko <steepdog3@gmail.com>
2021-07-17 23:11:57 +0300

    feat: add test 19

---
+++ index19.js
@@ -0,0 +1,17 @@
+// Практика JS 19.
+// Если число четное, преобразуйте его в двоичное. Если число нечетное, преобразуйте его в шестнадцатеричное.
+
+// Пример кода:
+//    evensAndOdds(2) => '10'
+//    evensAndOdds(13) => 'd'
+
+function evensAndOdds(number){
+  if(number%2 === 0){
+    return String((number >>> 0).toString(2));
+  } else{
+    return (number >>> 0).toString(16);
+  }
+}
+
+console.log(evensAndOdds(2))
+console.log(evensAndOdds(13))
